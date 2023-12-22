4154d6feb270ae3d5effe9b4eb86d3312ff80b65
Guilherme Kochepki <gui.kochepki@gmail.com>
2023-12-22 15:52:35 -0300

    feat: Implementing background image for movie details

--- js/script.js
+++ js/script.js
@@ -75,6 +75,9 @@ async function displayMovieDetails() {
 
     const movie = await fetchAPIData(`movie/${movieId}`);
 
+    // Overlay for background image
+    displayBackgroundImage('movie', movie.backdrop_path);
+
     const div = document.createElement('div');
 
     div.innerHTML = `
@@ -128,6 +131,28 @@ async function displayMovieDetails() {
     `
 
     document.querySelector('#movie-details').appendChild(div)
+}
+
+// Display Backdrop On Details Pages
+function displayBackgroundImage(type, backgroudPath) {
+    const overlayDiv = document.createElement('div');
+    overlayDiv.style.backgroundImage = `url(https://image.tmdb.org/t/p/original/${backgroudPath})`;
+    overlayDiv.style.backgroundSize = 'cover';
+    overlayDiv.style.backgroundPosition = 'center;'
+    overlayDiv.style.backgroundRepeat = 'no-repeat';
+    overlayDiv.style.height = '100vh';
+    overlayDiv.style.width = '100vw';
+    overlayDiv.style.position = 'absolute';
+    overlayDiv.style.top = '0';
+    overlayDiv.style.left = '0';
+    overlayDiv.style.zIndex = '-1';
+    overlayDiv.style.opacity = '0.1';
+
+    if (type === 'movie') {
+        document.querySelector('#movie-details').appendChild(overlayDiv);
+    } else {
+        document.querySelector('#show-details').appendChild(overlayDiv);
+    }
 }
 
 //Fetch data from TMDB API
